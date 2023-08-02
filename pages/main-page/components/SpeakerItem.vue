37d761578ad33da0e0d93215c0f6b10179205b66
mikhail-frontend <mikhail.kharitonov2022.frontend@gmail.com>
2023-08-02 18:29:47 +0300

    added the basic slider

---
+++ pages/main-page/components/SpeakerItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="speaker">
+    <img :src="speaker.image" :alt="speaker.name" class="speaker__image">
+    <div class="speaker__info">
+      <div class="speaker__name">{{speaker.name}}</div>
+      <div class="speaker__extra">{{speaker.extra_text}}</div>
+      <div class="speaker__description" v-html="speaker.description"/>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import {Component, Vue, Prop} from "nuxt-property-decorator";
+@Component
+export default class SpeakerItem extends Vue{
+  @Prop({type: Object, default: () => ({})}) speaker;
+
+}
+</script>
+<style lang="scss" scoped>
+@import "../styles/SpeakerItem";
+</style>
--- pages/main-page/components/SpeakersSection.vue
+++ pages/main-page/components/SpeakersSection.vue
@@ -2,13 +2,50 @@
   <section class="speakers-section physics-container">
     <header class="speakers-section__header">День физики —</header>
     <h2 class="speakers-section__heading">Самые известные спикеры</h2>
+
+    <div class="speakers-slider">
+      <ssr-carousel
+          :slides-per-page="3"
+          :gutter="45"
+          :responsive="[
+              {
+                maxWidth: 800,
+                slidesPerPage: 2,
+              },
+
+              {
+                maxWidth: 600,
+                gutter: 0,
+                slidesPerPage: 1,
+              }
+      ]"
+      >
+        <div  v-for="speaker in speakers"
+              :key="speaker.id">
+          <speaker-item :speaker="speaker"
+          />
+        </div>
+
+      </ssr-carousel>
+
+    </div>
   </section>
 </template>
 <script lang="ts">
 import {Component, Vue} from "nuxt-property-decorator";
-@Component
-export default class SpeakersSection extends Vue{
+import speakers from "../entities/speakers";
+import SpeakerItem from "./SpeakerItem.vue";
+// @ts-ignore
+import SsrCarousel from 'vue-ssr-carousel';
 
+@Component({
+  components: {
+    SpeakerItem,
+    SsrCarousel
+  }
+})
+export default class SpeakersSection extends Vue {
+  speakers = speakers;
 }
 </script>
 <style lang="scss" scoped>
