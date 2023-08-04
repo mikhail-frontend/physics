b08623e65641c7d8cccb4519b3297352e5d6c22f
mikhail-frontend <mikhail.kharitonov2022.frontend@gmail.com>
2023-08-04 14:12:45 +0300

    added cities rendering

--- pages/main-page/components/YandexMap.vue
+++ pages/main-page/components/YandexMap.vue
@@ -1,6 +1,22 @@
 <template>
-  <div v-if="!loading">
-    <pre>{{citiesWithSchools}}</pre>
+  <div v-if="!loading" class="yandex-map">
+    <div class="yandex-map__cities">
+      <div class="map-cities__title">Города</div>
+      <div class="profile-radios">
+        <div class="profile-radio" v-for="(city) in citiesWithSchools" :key="city.id">
+
+          <input type="radio"
+                 name="profile"
+                 :id="city.id"
+                 :value="city.id"
+                 :checked="checked_city === city.id"
+                 class="profile-radio__input"
+          />
+          <label :for="city.id">{{city.title}}</label>
+        </div>
+      </div>
+    </div>
+    <div id="map" class="yandex-map__map"></div>
   </div>
 </template>
 <script lang="ts">
@@ -12,17 +28,18 @@ export default class YandexMap extends Vue {
 
   loading = true;
   citiesWithSchools = [];
+  checked_city = 0;
+  map_canter = [];
 
-  mounted() {
-    this.getSchools();
+  async mounted() {
+    await this.getSchools();
+    this.setData();
     const {ymaps} = window;
-
-
   }
 
   async getSchools() {
+    this.loading = true;
     try {
-      this.loading = true;
       const {data:serverResponse} = await axios.get(
           `http://localhost:8080/api/public/companies/1/map_cities`,
           {
@@ -34,11 +51,18 @@ export default class YandexMap extends Vue {
       );
       const {data} = serverResponse;
       this.$set(this, 'citiesWithSchools', data);
-      this.loading = false;
     } catch (e) {
       this.$set(this, 'citiesWithSchools', universities);
-      this.loading = false;
     }
+  }
+
+  setData() {
+    const {citiesWithSchools} = this;
+    if(!citiesWithSchools.length) return;
+    const [first] = citiesWithSchools;
+    this.checked_city = first.id;
+    this.map_canter = first.city_coords;
+    this.loading = false
   }
 }
 </script>
